920eb97d05dd4b40a44cc94593ed3ff052519c29
Brooks Nuss <bnussjr@gmail.com>
2023-07-13 22:18:12 -0400

    Added balls and boundaries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { BallComponent } from './ball/ball.component';
 import { PhysicsService } from './services/physics.service';
+import { Bodies } from 'matter-js';
 
 @Component({
 	selector: 'app-root',
@@ -11,6 +12,7 @@ export class AppComponent implements AfterViewInit {
 	@ViewChild('gameArea', { read: ElementRef }) gameAreaElement: ElementRef;
 	title = 'poolwithbrooksie';
 	balls: BallComponent[] = [];
+	colors: ['blue', 'yellow', 'red', 'orange', ]
 
 	constructor(private physicsService: PhysicsService) {
 		for (let i = 0; i < 15; i++) {
@@ -28,7 +30,21 @@ export class AppComponent implements AfterViewInit {
 
 	ngAfterViewInit(): void {
 		this.physicsService.renderElement = this.gameAreaElement.nativeElement;
-		// this.physicsService.addStuff();
+		const ballArr = [];
+		for (let i = 1; i < 16; i++) {
+			const texture = '../assets/poolSprites/' + i + '.png';
+			const ball = Bodies.circle(i * 30 + 300, 50 + 300, 15, { render: { sprite: { texture, xScale: 0.01067, yScale: 0.01067 }}});
+			this.physicsService.addBody(ball);
+			// const ball=new BallComponent();
+			// let category="solid"
+			// if (i+1 > 8) {
+			// 	category="striped";
+			// }
+			// ball.category = category;
+			// ball.number = i + 1;
+			// this.balls.push(ball);
+		}
+		this.viewBalls()
 	}
 
 	viewBalls(): void {
