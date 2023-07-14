84d6612a598b0422f7054b6c89d5c01fef326c1c
Brooks Nuss <bnussjr@gmail.com>
2023-07-14 01:31:08 -0400

    Ball placement and physics tweaking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { BallComponent } from './ball/ball.component';
 import { PhysicsService } from './services/physics.service';
-import { Bodies } from 'matter-js';
+import { Bodies, Body, Composite, Composites, IBodyDefinition } from 'matter-js';
 
 @Component({
 	selector: 'app-root',
@@ -12,45 +12,79 @@ export class AppComponent implements AfterViewInit {
 	@ViewChild('gameArea', { read: ElementRef }) gameAreaElement: ElementRef;
 	title = 'poolwithbrooksie';
 	balls: BallComponent[] = [];
-	colors: ['blue', 'yellow', 'red', 'orange', ]
+	private ballCount = 0;
 
 	constructor(private physicsService: PhysicsService) {
-		for (let i = 0; i < 15; i++) {
-			const ball=new BallComponent();
-			let category="solid"
-			if (i+1 > 8) {
-				category="striped";
-			}
-			ball.category = category;
-			ball.number = i + 1;
-			this.balls.push(ball);
-		}
-		this.viewBalls()
+		// for (let i = 0; i < 15; i++) {
+		// 	const ball=new BallComponent();
+		// 	let category="solid"
+		// 	if (i+1 > 8) {
+		// 		category="striped";
+		// 	}
+		// 	ball.category = category;
+		// 	ball.number = i + 1;
+		// 	this.balls.push(ball);
+		// }
 	}
 
 	ngAfterViewInit(): void {
 		this.physicsService.renderElement = this.gameAreaElement.nativeElement;
-		const ballArr = [];
-		for (let i = 1; i < 16; i++) {
-			const texture = '../assets/poolSprites/' + i + '.png';
-			const ball = Bodies.circle(i * 30 + 300, 50 + 300, 15, { render: { sprite: { texture, xScale: 0.01067, yScale: 0.01067 }}});
-			this.physicsService.addBody(ball);
-			// const ball=new BallComponent();
-			// let category="solid"
-			// if (i+1 > 8) {
-			// 	category="striped";
-			// }
-			// ball.category = category;
-			// ball.number = i + 1;
-			// this.balls.push(ball);
+		const xStart = 750;
+		const yStart = 645;
+		const ballArr: Composite[] = [];
+		// const balls = Composites.pyramid(1020, 645, 11, 11, 0, 0, this.getNextBall);
+		for (let i = 1; i < 6; i++) {
+			ballArr.push(Composites.stack(xStart - 30 * i, yStart - 15 * i, 1, i, 0, 0, this.getNextBall));
 		}
-		this.viewBalls()
+		const cue = this.getNextBall(1400, 645);
+		// const balls1 = Composites.stack(xStart, yStart)
+		// const balls5 = Composites.stack(1020, 645, 1, 5, 0, 0, this.getNextBall);
+		// for (let i = 1; i < 16; i++) {
+		// 	const texture = '../assets/poolSprites/' + i + '.png';
+		// 	const ball = Bodies.circle(i * 30 + 300, 50 + 300, 15, { frictionAir: 0.02, render: { sprite: { texture, xScale: 0.01067, yScale: 0.01067 }}, restitution: 0.5 });
+		// 	this.physicsService.addBody(ball);
+		// 	// const ball=new BallComponent();
+		// 	// let category="solid"
+		// 	// if (i+1 > 8) {
+		// 	// 	category="striped";
+		// 	// }
+		// 	// ball.category = category;
+		// 	// ball.number = i + 1;
+		// 	// this.balls.push(ball);
+		// }
+		ballArr.forEach(ballComposite => {
+			this.physicsService.addComposite(ballComposite);
+		})
+		this.physicsService.addBody(cue);
 	}
 
-	viewBalls(): void {
-		console.log(this.balls);
+	private getNextBall = (x: number, y: number): Body => {
+		const generatedValue = this.createBall(x, y, this.ballCount + 1);
+		this.ballCount++;
+		return generatedValue;
 	}
-	// if (ball.isSleeping) {
-	//   ball.setStatic(true);
-	// }
+
+	private createBall(x: number, y: number, i: number): Body {
+		const ballOptions: IBodyDefinition = {
+			frictionAir: 0.01,
+			render: {
+				sprite: {
+					texture: '',
+					xScale: 0.01067,
+					yScale: 0.01067
+				}
+			},
+			restitution: 1,
+			density: 1700
+		};
+		if (i < 16) {
+			const texture = '../assets/poolSprites/' + i + '.png';
+			ballOptions.render!.sprite!.texture = texture;
+			const ball = Bodies.circle(x, y, 15, ballOptions);
+			return ball;
+		}
+		delete (ballOptions.render!.sprite);
+		ballOptions.render!.fillStyle = 'white';
+		return Bodies.circle(x, y, 15, ballOptions);
+	}
 }
